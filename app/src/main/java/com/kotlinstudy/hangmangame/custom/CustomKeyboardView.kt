58f9a7c1bf2137cea1a81036a12c9b1cc2714a89
Angelica de Oliveira <angelica@lnxcit016547.cit>
2017-10-26 17:48:40 -0200

    Componente teclado

---
+++ app/src/main/java/com/kotlinstudy/hangmangame/custom/CustomKeyboardView.kt
@@ -0,0 +1,12 @@
+package com.kotlinstudy.hangmangame.custom
+
+import android.content.Context
+import android.widget.LinearLayout
+
+/**
+ * Created by angelica on 10/26/17.
+ */
+
+class CustomKeyboardView(context: Context) : LinearLayout(context) {
+
+}
